600e98f234a645293c3f8f228d03a77e98a6206f
ccut <christophercutajar@gmail.com>
2014-06-30 12:00:24 +0000

    Server header file changes

--- Server/tcp_server.h
+++ Server/tcp_server.h
@@ -7,13 +7,4 @@ typedef struct connections
     unsigned long ip;	
 }connections;
 
-typedef struct file
-{
-    int fid;
-    char * pathname;
-    FILE * fd;
-}file;
-
-void openFile(char f[]);
-void closeFile(FILE * f);
 #endif // TCP_SERVER_H
