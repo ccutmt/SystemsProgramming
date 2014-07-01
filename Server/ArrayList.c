efd5a8d423d68cf419e81c08e9ac21a9eaf57e3d
ccut <christophercutajar@gmail.com>
2014-07-01 10:33:37 +0000

    Error Messages and other minor changes

--- Server/ArrayList.c
+++ Server/ArrayList.c
@@ -3,30 +3,25 @@
 #include <string.h>
 #include "ArrayList.h"
 
-void initArrayList (ArrayList *const list)
-{
+void initArrayList (ArrayList *const list){
 	list->size = 2;
 	list->elements = (void *) calloc(list->size, sizeof(void *));
 	list->current = -1;
 }
 
-void add (ArrayList *const list, void * e)
-{
-	if (list->current < list->size-1)
-	{
+void add (ArrayList *const list, void * e){
+	if (list->current < list->size-1){
 		list->current++;
 		list->elements[list->current] = e;
 	}
-	else
-	{
+	else{
 		resize(list);
 		list->current++;
 		list->elements[list->current] = e;
 	}
 }
 
-void resize(ArrayList* const list)
-{
+void resize(ArrayList* const list){
 	list->size *= 2;
 	void *newArr = malloc(sizeof(void *) * list->size);
 	memcpy(newArr,list->elements, (list->current+1) * sizeof(void *));
@@ -34,31 +29,25 @@ void resize(ArrayList* const list)
 	list->elements = newArr;
 }
 
-int isEmpty (const ArrayList *const list)
-{
+int isEmpty (const ArrayList *const list){
 	return list->current == -1;
 }
 
-void removeAt(ArrayList *const list, int index)
-{
-	if (index <= list->current)
-	{
+void removeAt(ArrayList *const list, int index){
+	if (index <= list->current){
 		memmove(&list->elements[index],&list->elements[index+1], ((list->current+1) - index) * sizeof(void *));
 		list->current--;
 	}
 }
 
-void freeAll(ArrayList *list)
-{
+void freeAll(ArrayList *list){
 	int i = 0;
  	for(i = 0; i <= list->current; i++)
         	free(getElement(list,i));
 }
 
-void * getElement (ArrayList *const list, int index)
-{
-    if (index <= list->current)
-    {
+void * getElement (ArrayList *const list, int index){
+    if (index <= list->current){
     	return list->elements[index];
     }
     
--- Server/files.c
+++ Server/files.c
@@ -1,39 +1,33 @@
 #include "files.h"
 #include "ArrayList.h"
 
-int openFile(char* pathname)
-{
+int openFile(char* pathname){
 	int fin = open(pathname, O_RDWR);
 	return fin;
 }
 
-int closeFile(int fd)
-{
+int closeFile(int fd){
 	return close(fd);
 }
 
-int readFile(char* buff, size_t length, int fd, off_t offset)
-{
+int readFile(char* buff, size_t length, int fd, off_t offset){
 	off_t check;
 	ssize_t bytes;
 
 	check = lseek(fd, offset, SEEK_SET);
 
-	if(check == -1)
-	{
+	if(check == -1){
 		perror("Seek error");
 		return -1;
 	}
 
 	bytes = read(fd, buff, length);
 
-	if(bytes == -1)
-	{
+	if(bytes == -1){
 		perror("Read error");
 		return -1;
 	}
-	else
-	{
+	else{
 		perror("it is 0");
 		return 0;
 	}
@@ -45,19 +39,19 @@ int writeFile(char *buff, size_t length, int fd, off_t offset){
 
 	check = lseek(fd, offset, SEEK_SET);
 
-	if(check == -1)
-	{
+	if(check == -1){
+		perror("Seek Error");
 		return -1;
 	}
 
 	bytes = write(fd, buff, length);
 
-	if(bytes == -1)
-	{
+	if(bytes == -1){
+		perror("Write Error");
 		return -1;
 	}
-	else
-	{
+	else{
+		perror("it is 0");
 		return 0;
 	}
 }
